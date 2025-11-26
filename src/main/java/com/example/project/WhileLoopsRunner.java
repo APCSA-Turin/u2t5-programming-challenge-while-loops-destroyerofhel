88c264e36e36fdc6a7c6e29fe28111d626a70652
destroyerofhel <156379798+destroyerofhel@users.noreply.github.com>
2025-11-26 18:27:05 +0000

    message

--- src/main/java/com/example/project/WhileLoopsRunner.java
+++ src/main/java/com/example/project/WhileLoopsRunner.java
@@ -4,6 +4,6 @@ package com.example.project;
 public class WhileLoopsRunner{
 
     public static void main(String[] args) {
-        System.out.println(WhileLoops.countDownBy(7, 10));
+        
     }
 }
